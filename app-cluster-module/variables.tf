b2be032199bb224504742b448e0e06865365de80
Yosef Ejigu <42782525+yoejigu@users.noreply.github.com>
2026-03-31 12:33:13 -0400

    Add variables for app cluster configuration in Terraform

---
+++ app-cluster-module/variables.tf
@@ -0,0 +1,30 @@
+variable "app_name" {
+  type        = string
+  description = "Name of the application (e.g. claims-processing)"
+}
+variable "environment" {
+  type        = string
+  description = "Deployment environment: dev, staging, or prod"
+}
+variable "resource_group_name" {
+  type        = string
+  description = "Azure resource group provisioned by the platform team"
+}
+variable "location" {
+  type        = string
+  default     = "eastus"
+  description = "Azure region"
+}
+variable "subnet_id" {
+  type        = string
+  description = "Subnet ID from the network team for AKS nodes"
+}
+variable "node_count" {
+  type        = number
+  default     = 2
+  description = "Number of worker nodes"
+}
+variable "kubernetes_version" {
+  type    = string
+  default = "1.28"
+}
